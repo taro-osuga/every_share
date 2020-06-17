47c55ccef8f2f32fb0d2c41c464249d877225009
taroosuga <copernix0@gmail.com>
2020-06-17 20:18:18 +0900

    モデルにhas_many記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     belongs_to :user
     has_many :photos
+    has_many :reservations
 
     geocoded_by :address
     after_validation :geocode, if: :address_changed?
