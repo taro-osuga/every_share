537c8985ddf0d3ca10f2472ad09fc7763ebaa974
taroosuga <copernix0@gmail.com>
2020-06-19 13:39:09 +0900

    レビューページ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
     has_many :photos
     has_many :reservations
 
+    has_many :guest_reviews
+
     geocoded_by :address
     after_validation :geocode, if: :address_changed?
 
@@ -16,5 +18,9 @@ class Item < ApplicationRecord
       else
         "blank.jpg"
       end
+    end
+
+    def average_rating
+      guest_reviews.count == 0 ? 0 : guest_reviews.average(:star).round(2).to_i
     end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [150, 150]
   end
 
 end
