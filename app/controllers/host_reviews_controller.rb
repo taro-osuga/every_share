10bfb70d60e3a78fe2739246bc03fa8d85fc0941
taroosuga <copernix0@gmail.com>
2020-06-26 22:40:42 +0900

    レイアウト修正

--- app/controllers/host_reviews_controller.rb
+++ app/controllers/host_reviews_controller.rb
@@ -1,19 +1,27 @@
 class HostReviewsController < ApplicationController
 
-    def create  
-      @reservation = Reservation.research.first
+    def create
+      @reservation = Reservation.where(
+        id: host_review_params[:reservation_id],
+        item_id: host_review_params[:item_id],
+        user_id: host_review_params[:guest_id]
+       ).first
   
       if !@reservation.nil?
-        @has_reviewed = HostReview.hostresearch.first
+  
+        @has_reviewed = HostReview.where(
+          reservation_id: @reservation.id,
+          guest_id: host_review_params[:guest_id]
+        ).first
   
         if @has_reviewed.nil?
             @host_review = current_user.host_reviews.create(host_review_params)
-            flash[:success] = "Review created..."
+            flash[:success] = "レビュー送信しました."
         else
-            flash[:success] = "You already reviewed this Reservation"
+            flash[:success] = "既にレビューがあります"
         end
       else
-        flash[:alert] = "Not found this reservation"
+        flash[:alert] = "予約が見つかりません"
       end
   
       redirect_back(fallback_location: request.referer)
