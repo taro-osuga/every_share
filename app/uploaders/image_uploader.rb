795c066bf63c6d7bccbd89631de0a9923a329c2b
taroosuga <copernix0@gmail.com>
2020-06-29 03:29:30 +0900

    レイアウト修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,14 +3,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :fog
 
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
